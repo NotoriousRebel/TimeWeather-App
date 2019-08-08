0470620649a125cb5e594e985dfa18cf5f8c4c7f
NotoriousRebel <matt12299@yahoo.com>
2019-08-08 12:41:55 -0400

    Updates

---
+++ precache-manifest.968d4ea91d9dfae1d59c28cc7a9a051c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20007005e006ffb4c6e6067ff58a0390",
+    "url": "/TimeWeather-App/index.html"
+  },
+  {
+    "revision": "2f50ad65c02a21a6d735",
+    "url": "/TimeWeather-App/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "6feb231a73414490f21c",
+    "url": "/TimeWeather-App/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2f50ad65c02a21a6d735",
+    "url": "/TimeWeather-App/static/js/2.d3be7aef.chunk.js"
+  },
+  {
+    "revision": "6feb231a73414490f21c",
+    "url": "/TimeWeather-App/static/js/main.8669475a.chunk.js"
+  },
+  {
+    "revision": "5efcdd6c3860629753d7",
+    "url": "/TimeWeather-App/static/js/runtime~main.2535e6a0.js"
+  }
+]);
